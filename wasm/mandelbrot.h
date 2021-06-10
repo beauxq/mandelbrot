484593d891ee57c86bb8d48d3e0b8c0f31d4b566
beauxq <beauxq@users.noreply.github.com>
2021-06-10 10:13:32 -0700

    added period checking algorithm but not using it because it didn't seem good

--- wasm/mandelbrot.h
+++ wasm/mandelbrot.h
@@ -1,5 +1,7 @@
 #include <emscripten/emscripten.h>
 
+#define MAX_ITERATION 512
+
 void f(double cr, double ci, double zr, double zi, double* outR, double* outI) {
     const double mr = zr * zr - zi * zi;
     const double mi = zr * zi * 2;
@@ -13,7 +15,7 @@ double countIterOld(double cr, double ci) {
     double zi = 0;
     int i = 0;
     double mz2 = 0;
-    while (mz2 <= 4 && i < 512) {
+    while (mz2 <= 4 && i < MAX_ITERATION) {
         f(cr, ci, zr, zi, &zr, &zi);
         mz2 = zr * zr + zi * zi;
         ++i;
@@ -28,11 +30,47 @@ double countIter(double cr, double ci) {
     double zr2 = 0;
     double zi2 = 0;
     int i = 0;
-    while (zr2 + zi2 <= 4 && i < 512) {
+    while (zr2 + zi2 <= 4 && i < MAX_ITERATION) {
         zi = 2.0 * zr * zi + ci;
         zr = zr2 - zi2 + cr;
         zr2 = zr * zr;
         zi2 = zi * zi;
+        ++i;
+    }
+    double mz2 = zr2 + zi2;
+    return i + 1 / ((mz2 < 5) ? 1 : mz2 - 4);
+}
+
+// with period checking
+// I think this is not worth it
+// not deleting because I didn't do any precise performance tests
+double countIterPeriod(double cr, double ci) {
+    double zr = 0;
+    double zi = 0;
+    double zr2 = 0;
+    double zi2 = 0;
+    double zrp = 0;
+    double zip = 0;
+    int period = 0;
+    int i = 0;
+    while (zr2 + zi2 <= 4 && i < MAX_ITERATION) {
+        zi = 2.0 * zr * zi + ci;
+        zr = zr2 - zi2 + cr;
+        zr2 = zr * zr;
+        zi2 = zi * zi;
+
+        if (zr == zrp && zi == zip) {
+            i = MAX_ITERATION;
+            break;
+        }
+
+        ++period;
+        if (period > 20) {
+            period = 0;
+            zrp = zr;
+            zip = zi;
+        }
+
         ++i;
     }
     double mz2 = zr2 + zi2;
