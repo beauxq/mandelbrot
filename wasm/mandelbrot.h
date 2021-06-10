3ba5795dafa7c5d3a7388148e0b3b73ddba33ff1
beauxq <beauxq@users.noreply.github.com>
2021-06-10 09:39:31 -0700

    optimize iteration counter

--- wasm/mandelbrot.h
+++ wasm/mandelbrot.h
@@ -1,7 +1,5 @@
 #include <emscripten/emscripten.h>
 
-// TODO: optimize: zr squared and zi squared are calculated extra times
-
 void f(double cr, double ci, double zr, double zi, double* outR, double* outI) {
     const double mr = zr * zr - zi * zi;
     const double mi = zr * zi * 2;
@@ -10,15 +8,33 @@ void f(double cr, double ci, double zr, double zi, double* outR, double* outI) {
 }
 
 /** how many iterations to get above 2 */
-double countIter(double cr, double ci) {
+double countIterOld(double cr, double ci) {
     double zr = 0;
     double zi = 0;
     int i = 0;
     double mz2 = 0;
-    while (mz2 <= 8 && i < 512) {
+    while (mz2 <= 4 && i < 512) {
         f(cr, ci, zr, zi, &zr, &zi);
         mz2 = zr * zr + zi * zi;
         ++i;
     }
-    return i + 1 / ((mz2 < 9) ? 1 : mz2 - 8);
+    return i + 1 / ((mz2 < 5) ? 1 : mz2 - 4);
+}
+
+// optimization from https://en.wikipedia.org/wiki/Plotting_algorithms_for_the_Mandelbrot_set
+double countIter(double cr, double ci) {
+    double zr = 0;
+    double zi = 0;
+    double zr2 = 0;
+    double zi2 = 0;
+    int i = 0;
+    while (zr2 + zi2 <= 4 && i < 512) {
+        zi = 2.0 * zr * zi + ci;
+        zr = zr2 - zi2 + cr;
+        zr2 = zr * zr;
+        zi2 = zi * zi;
+        ++i;
+    }
+    double mz2 = zr2 + zi2;
+    return i + 1 / ((mz2 < 5) ? 1 : mz2 - 4);
 }
